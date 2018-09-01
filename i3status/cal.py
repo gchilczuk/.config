c15870c556fef637161ce1da1c6bcb4c4566e1f0
Grzegorz Chilczuk <chilczukg@gmail.com>
2018-09-01 19:28:36 +0200

    init

---
+++ i3status/cal.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+import subprocess
+
+
+class Py3status:
+
+    def __init__(self):
+        self.full_text = 'K'
+
+    def click_info(self):
+        return {
+            'full_text': self.full_text,
+            'cached_until': self.py3.CACHE_FOREVER
+        }
+
+    def on_click(self, event):
+        subprocess.call(["gsimplecal"])
---
+++ i3status/hello.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+from random import randint
+
+class Py3status:
+
+    def __init__(self):
+        self.full_text = 'Dice'
+
+    def click_info(self):
+        return {
+            'full_text': self.full_text,
+            'cached_until': self.py3.CACHE_FOREVER
+        }
+
+    def on_click(self, event):
+        rn = randint(0,9)
+        format_string = 'Dice: {random_number}'
+        data = {'random_number': rn}
+        self.full_text = self.py3.safe_format(format_string, data)
