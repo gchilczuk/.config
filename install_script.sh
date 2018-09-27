1f17556bc7becd7dcd751388a963634aa78703d2
Grzegorz Chilczuk <chilczukg@gmail.com>
2018-09-28 00:15:15 +0200

    fix installation (not)script commands order and sudo usage

--- install_script.sh
+++ install_script.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
-apt update
+# may not work correctly as a script
+
+sudo apt update
+
+# install deps
+sudo apt install -y git make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev python3-dev
 
 # install zsh
-apt install -y zsh
-chsh -s /usr/bin/zsh root
+sudo apt install -y zsh
+chsh -s /usr/bin/zsh root # change root to your username
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
 # install pyenv
@@ -16,49 +21,39 @@ echo -e 'if command -v pyenv 1>/dev/null 2>&1; then\n  eval "$(pyenv init -)"\nf
 git clone https://github.com/pyenv/pyenv-virtualenv.git $(pyenv root)/plugins/pyenv-virtualenv
 echo 'eval "$(pyenv virtualenv-init -)"' >> ~/.zshenv
 
-exec "$SHELL"
-
-apt install -y make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev
-
 pyenv install 3.7.0
 pyenv virtualenv 3.7.0 global3.7
 pyenv global global3.7
 
 # i3 environment
-apt install -y i3
-apt install -y Thunar
-apt install -y feh
+sudo apt install -y i3 py3status rofi thunar feh blueman-applet lxappearance pavucontrol gsimplecal
+
+#
+# There is a moment to logout and login again with i3
+# 
 
 # other must have
-apt install -y htop 
-apt install -y redshift redshift-gtk 
-apt install -y copyq
-apt install -y shutter
-apt install -y okular
-apt install -y kazam
-apt install -y cheese
-apt install -y vlc
+sudo apt install -y htop okular
+sudo apt install -y redshift redshift-gtk copyq shutter kazam cheese clementine vlc
+pip install thefuck
 
-wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | apt-key add -
-apt install apt-transport-https
-echo "deb https://download.sublimetext.com/ apt/stable/" | tee /etc/apt/sources.list.d/sublime-text.list
-apt update
-apt install -y sublime-text
+wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
+sudo apt install apt-transport-https
+echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
+sudo apt update
+sudo apt install -y sublime-text
 
-add-apt-repository ppa:micahflee/ppa
-apt update
-apt install -y onionshare
+sudo add-apt-repository ppa:micahflee/ppa
+sudo apt update
+sudo apt install -y onionshare
 
-apt install -y python3-dev python3-pip
-pip install -y thefuck
+sudo add-apt-repository ppa:keepassx/ppa
+sudo apt update
+sudo apt install -y keepassx
 
-add-apt-repository ppa:keepassx/ppa
-apt update
-apt install -y keepassx
-
-apt-add-repository ppa:neovim-ppa/stable
-apt update
-apt install neovim
+sudo apt-add-repository ppa:neovim-ppa/stable
+sudo apt update
+sudo apt install neovim
 
 # install snaps
 snap install tldr
@@ -69,7 +64,7 @@ snap install qalculate
 cp ./i3/config ~/.config/i3/config
 cp ./i3/lock.sh ~/.config/i3/lock.sh
 cp ./bin/wallpaper.png ~/.config/i3/wallpaper.png
-cp ./i3status/ ~/.config/i3status/
+cp -r ./i3status/ ~/.config/i3status/
 cp ./rofi/config.rasi ~/.config/rofi/config.rasi 
-cp ./zshrc ~/.zshrc
-cp ./bin/calc ./bin/*.sh /usr/loca/bin/
+cp ./.zshrc ~/.zshrc
+sudo cp ./bin/calc ./bin/*.sh /usr/loca/bin/
