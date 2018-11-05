e8f962fd47db27b82ea5fb883cb1e1b4973cd308
Grzegorz Chilczuk <chgrzegorz@pm.me>
2018-11-05 22:30:16 +0100

    fix mute toggle; udpate i3 config

--- bin/toggle_mute_sound.sh
+++ bin/toggle_mute_sound.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-for kar in $(pactl list | grep karta | cut -d'.' -f1); do pactl set-sink-volume $kar toggle; done
+for kar in $(pactl list | grep karta | cut -d'.' -f1); do pactl set-sink-mute $kar toggle; done
